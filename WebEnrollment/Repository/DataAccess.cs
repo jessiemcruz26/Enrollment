e285252fb600bae17ba04d0637f6e1899a822119
jessiemcruz26 <106714722+jessiemcruz26@users.noreply.github.com>
2022-06-19 14:48:13 -0400

    Updated dataaccess

--- WebEnrollment/Repository/DataAccess.cs
+++ WebEnrollment/Repository/DataAccess.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Web;
 
 namespace WebEnrollment.Repository
@@ -17,10 +21,54 @@ namespace WebEnrollment.Repository
         //{
         //    _userProvider = userProvider;
         //}
+
         public List<string> Get()
         {
             //var user = _userProvider.UserName;
             return new List<string> { "User 1", "User 2", "User 3" };
         }
+
+        public static HttpClient ApiClient { get; set; } = new HttpClient();
+
+        public static void Initialize()
+        {
+            Get1();
+            Post();
+        }
+
+        public static async void Get1()
+        {
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            string uri = "http://localhost:63908/api/values/1/";
+            using (HttpResponseMessage reponse = await ApiClient.GetAsync(uri))
+            {
+                string a = await reponse.Content.ReadAsStringAsync();
+            }
+        }
+        public class Profile
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+        public static async void Post()
+        {
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+
+            Profile prof = new Profile();
+            prof.FirstName = "Jessie";
+            prof.LastName = "Cruz";
+
+            var uri = string.Format("http://localhost:63908/api/values/");
+
+            var content = new StringContent(JsonConvert.SerializeObject(prof), Encoding.UTF8, "application/json");
+
+            using (HttpResponseMessage reponse = await ApiClient.PostAsync(uri, content))
+            {
+                var a = await reponse.Content.ReadAsStringAsync();
+            }
+        }
+
     }
 }
